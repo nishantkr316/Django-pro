b4903e8301f12312d25d8d5a4678d57c51d3c605
my name <nraj6014@gmail.com>
2026-01-12 19:54:24 +0530

    add some features

--- ORMProject/myapp/urls.py
+++ ORMProject/myapp/urls.py
@@ -4,5 +4,6 @@ from myapp import views
 urlpatterns=[
     
     path('data/',views.retrive_data),
-    path('filter/',views.fiter_data)
+    path('filter/',views.fiter_data),
+    path('agg/',views.aggregate_view)
 ]
--- ORMProject/myapp/views.py
+++ ORMProject/myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from myapp.models import EmployeeModel
-from django.db.models import Q
+from django.db.models import Q,Max,Min,Avg,Sum,Count
 
 # Create your views here.
 
@@ -12,7 +12,21 @@ def retrive_data(request):
 def fiter_data(request):
     all_data=EmployeeModel.objects.filter(salary=45157)
     all_data=EmployeeModel.objects.filter (Q(id__range=[10,15]) | Q(salary__gt =50000))
-    # all_data=EmployeeModel.objects.filter(salary__gte = 60000)
+    all_data=EmployeeModel.objects.filter(salary__gte = 60000)
     context={"employee_data":all_data}
     return render(request,'filter.html',context)
 
+def aggregate_view(request):
+    max=EmployeeModel.objects.aggregate(max_sal=Max('salary'))
+    min=EmployeeModel.objects.aggregate(min_sal=Min('salary'))
+    avg=EmployeeModel.objects.aggregate(avg_sal=Avg('salary'))
+    sum=EmployeeModel.objects.aggregate(sum_sal=Sum('salary'))
+    count=EmployeeModel.objects.aggregate(count_sal=Count('salary'))
+    context={
+        'max':max,
+        'min':min,
+        'avg':avg,
+        'sum':sum,
+        'count':count
+    }
+    return render (request,'aggregate_data.html',context)
